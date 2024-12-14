d552af63b656741ef7d90a7c0052c2ce91f4716e
AliKhadr <ali.khadr98@hotmail.com>
2024-12-14 16:02:22 +0200

    Looked Lazy loading within relationship queries and used Eager loading to improve performance and N+1 Problem

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,13 +1,19 @@
 <x-layout>
     <x-slot:heading>Jobs Page</x-slot:heading>
     <h1>Welcome to the Jobs Page</h1><br>
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $item)
-        <li>
-            <a href="/jobs/{{$item['id']}}" class="text-blue-500 hover:underline">
+        <a href="/jobs/{{$item['id']}}" class="block px-4 py-6 border border-grey-200 rounded-lg">
+            <div class="font-bold text-blue-500 text-sm">{{$item->employer->name}}</div>
+            <div>
                 <strong>{{$item['Job_Title']}}</strong> => {{$item['Job_Salary']}} per year
-            </a>
-        </li>
+            </div>
+            <div>
+                @foreach ($item->tags as $tag)
+                    <span>{{$tag->name}}</span>
+                @endforeach
+            </div>
+        </a>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
